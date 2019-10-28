a5a54261845d476354906400bc431895dbaab645
yunakagawa <ynakagawau@gmail.com>
2019-10-28 14:57:13 +0000

    sendgrid setting4

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -93,6 +93,7 @@ Rails.application.configure do
   config.active_record.dump_schema_after_migration = false
   config.action_mailer.default_url_options = { host: 'meshistagram.herokuapp.com' }
   config.action_mailer.delivery_method = :smtp
+  config.action_mailer.perform_deliveries = true
   config.action_mailer.smtp_settings = {
   :enable_starttls_auto => true,
   :address => "smtp.sendgrid.net",
